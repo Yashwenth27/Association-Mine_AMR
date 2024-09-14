a348f0b9367a4a3d214017502e21892a7858222a
Yashwenth S <127986257+Yashwenth27@users.noreply.github.com>
2024-09-15 00:32:21 +0530

    Update association_query.py

--- association_query.py
+++ association_query.py
@@ -812,7 +812,7 @@ def set_age(org, b):
             st.subheader("Association Rules Query Panel")
             st.write("---")
             st.error("No Rules generated for this parameter configuration.")
-             st.warning("Try with new parameters!")
+            st.warning("Try with new parameters!")
             
 
 def set_year(org, b):
