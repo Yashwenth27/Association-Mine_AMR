36a390ae84c6b8e4ea8fc6dfb99961f0dc07dc0d
Yashwenth S <127986257+Yashwenth27@users.noreply.github.com>
2024-09-15 01:24:09 +0530

    Update association_query.py

--- association_query.py
+++ association_query.py
@@ -77,11 +77,11 @@ navbar_html = '''
 </svg>
     </div>
     <div class="menu">
-        <a href="">Home</a>
+        <a href="https://merit-sastra.streamlit.app/">Home</a>
         <a href="https://merit-dashboard.streamlit.app/" target="_blank">Dataset</a>
-        <a href="https://merit-mining.streamlit.app/" target="_self">Association Mining</a>
-        <a href="https://mining-query.streamlit.app/" target="_self">Track MDR</a>
-        <a href="https://merit-dataset.streamlit.app/" target="_self">Antimicrobial Trend</a>
+        <a href="https://merit-mining.streamlit.app/" target="_blank">Association Mining</a>
+        <a href="https://mining-query.streamlit.app/" target="_blank">Track MDR</a>
+        <a href="https://merit-dataset.streamlit.app/" target="_blank">Antimicrobial Trend</a>
         <a href="">About Us</a>
     </div>
 </nav>
